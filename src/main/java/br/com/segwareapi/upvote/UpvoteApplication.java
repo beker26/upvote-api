72515c5c7456ea11aba2cb9a2872e272f0ef5f0e
beker26 <rodrigobeker18@gmail.com>
2021-06-02 10:59:42 -0300

    Inicializando repositorio upvote!

---
+++ src/main/java/br/com/segwareapi/upvote/UpvoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.segwareapi.upvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpvoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/segwareapi/upvote/UpvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.segwareapi.upvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
